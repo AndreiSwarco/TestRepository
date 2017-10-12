fe3e4399cbf777f8e35083f3d29186eef32077f8
AndreiSwarco <andrei.iancu@swarco.com>
2017-10-12 16:13:42 +0300

    commit from second branch

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine($"C# prog");
             Console.WriteLine($"another line");
+
+            Console.WriteLine("from branches");
         }
     }
 }
